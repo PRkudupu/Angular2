d6f2ed003bcc46d8ce494b0e7c7a833c45d0af25
PRATHAP KUDUPU <PRATHAP KUDUPU>
2016-12-25 19:29:58 -0800

    User component is strongly typed

--- app/user.component.ts
+++ app/user.component.ts
@@ -4,21 +4,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user',
   template: `<h1>Simple Angular 2 template </h1>
-  <p> <strong>First Name : </strong>{{Firstname}} </p>
-  <p> <strong>Last Name : </strong>{{Lastname}} </p>
+  <p> <strong>First Name : </strong>{{firstName}} </p>
+  <p> <strong>Last Name : </strong>{{lastName}} </p>
   <p> <strong>MI  : </strong>{{MI}}</p>
   <p> <strong>DOB : </strong>{{DOB}}</p>
-  <p> <strong>Address :  </strong>{{Address.Street}} {{Address.City}} {{Address.State}} </p>
+  <p> <strong>address :  </strong>{{address.Street}} {{address.City}} {{address.State}} </p>
     `,
 })
 //Angular Component
 export class UserComponent  { 
-  Firstname = 'Prathap';
-  Lastname="Kudupu";
-  MI="H";
-  DOB="01/12/2000";
-  Address={
-    Street:"Vallco Pkwy",
-    City:"Cupertino",
-    State:"CA"};
+  firstName :string;
+  lastName :string;
+  MI:string;
+  DOB:string;
+  address:address
+    
+//Constructor
+    constructor(){
+    this.firstName = 'Prathap';
+    this.lastName="Kudupu";
+    this.MI="H";
+    this.DOB="01/12/2000";
+    this.address={
+                    Street:"Vallco Pkwy",
+                    City:"Cupertino",
+                    State:"CA"
+    }
+  }
 }
+//interface address{
+interface address{
+                    Street:string,
+                    City:string,
+                    State:string
+}
+
+
