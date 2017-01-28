54ea0f9ec540537d9b417c66a81d0d8595e0dbe0
PRATHAP KUDUPU <PRATHAP KUDUPU>
2017-01-27 18:29:25 -0800

    Services has been implemented

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,16 @@
 //import angular core
 import { Component } from '@angular/core';
+//import product service
+import { ProductService } from './products/product.service';
 //Decorator
+//In Providers property we specify the service
 @Component({
   selector: 'my-app',
   template: `
     <div><h1>{{pageTitle}}</h1></div>
     <pm-products></pm-products>
     `,
+  providers:[ProductService]
 })
 //Angular Component
 export class AppComponent  { 
