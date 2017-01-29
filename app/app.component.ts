340626fcb4ac1c39fa9befd9e12f6501575bd74b
PRATHAP KUDUPU <PRATHAP KUDUPU>
2017-01-28 20:56:57 -0800

    Router module has been added

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,24 @@ import { ProductService } from './products/product.service';
 //Decorator
 //In Providers property we specify the service
 @Component({
-  selector: 'my-app',
-  template: `
-    <div><h1>{{pageTitle}}</h1></div>
-    <pm-products></pm-products>
-    `,
-  providers:[ProductService]
+    selector: 'pm-app',
+    template: `
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+     `,
+    providers: [ ProductService ]
 })
 //Angular Component
 export class AppComponent  { 
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,26 +1,49 @@
 import { NgModule }      from '@angular/core';
+//Browser module
 import { BrowserModule } from '@angular/platform-browser';
 //form module
 import {FormsModule} from '@angular/forms'
 //http module
 import {HttpModule} from '@angular/http'
+//Router module
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+//User component
 import { UserComponent }  from './user.component';
+//Product list component
 import { ProductListComponent }  from './products/product-list.component';
+//Product detail component
+import { ProductDetailComponent }  from './products/product-detail.component';
+//Product filter component
 import { ProductFilterPipe }  from './products/product-filter.pipe';
+//star component
 import { StarComponent }  from './shared/star.component';
-
+//Welcome component
+import { WelcomeComponent }  from './home/welcome.component';
 
 @NgModule({
-  imports:      [ BrowserModule,
-                  FormsModule,
-                  HttpModule],
-  declarations: [ AppComponent , 
-                  UserComponent,
-                  ProductListComponent,
-                  ProductFilterPipe,
-                  StarComponent],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    //specify route path
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductFilterPipe,
+    StarComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
