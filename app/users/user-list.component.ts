659db60a732c17317f12b153982ee6fdcbd17f91
PRATHAP KUDUPU <PRATHAP KUDUPU>
2017-03-05 15:48:25 -0800

    Form controls and css class has been added.

--- app/users/user-list.component.ts
+++ app/users/user-list.component.ts
@@ -1,48 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 //Import interface
 import {IUser} from './user';
-//Import service
-
 
 @Component({
-    templateUrl: 'app/users/user-list.component.html'
+    templateUrl: 'app/users/user-list.component.html',
+    styleUrls:['app/users/user-list.component.css']
 })
 //Angular Component
 export class UserComponent  { 
   firstName :string;
   lastName :string;
   MI:string;
-  DOB:string;
-  address:address;
-  hobbies:string []; 
-  showhobbies:boolean; 
-  
+   
   //Constructor
     constructor(){
     this.firstName = 'Prathap';
     this.lastName="Kudupu";
     this.MI="H";
-    this.DOB="01/12/2000";
-    this.address={
-                    Street:"Vallco Pkwy",
-                    City:"Cupertino",
-                    State:"CA"
-    };
-    this.hobbies=['Music','Movies','Sports' ]
-    this.showhobbies=false;
+     }
+  addUsers(){
+    console.log(event);
+    //console.log(this.userForm.value);
   }
-  //functions
-  togglehobbies(){
-      if(this.showhobbies==true){
-      this.showhobbies=false;}
-      else{
-      this.showhobbies=true;
-      }
-  }
-  
-}
-interface address{
-                    Street:string,
-                    City:string,
-                    State:string
-}
+ }
+
--- app/users/user.ts
+++ app/users/user.ts
@@ -2,13 +2,4 @@ export interface IUser  {
   firstName :string;
   lastName :string;
   MI:string;
-  DOB:string;
-  address:address;
-  hobbies:string []; 
-  showhobbies:boolean; 
-}
-interface address{
-                    Street:string,
-                    City:string,
-                    State:string
 }
